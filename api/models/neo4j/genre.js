4375d8e82064009d1e40807a5f32bfcd2e6c46f4
Cristina <ce@tsl.io>
2016-02-29 01:29:50 -0500

    Removing angular version

--- api/models/neo4j/genre.js
+++
@@ -1,7 +0,0 @@
-// extracts just the data from the query results
-
-var _ = require('underscore');
-
-var Genre = module.exports = function (_node) {
-  _(this).extend(_node.data);
-};
--- api/models/neo4j/person.js
+++
@@ -1,8 +0,0 @@
-// extracts just the data from the query results
-
-var _ = require('underscore');
-
-var Person = module.exports = function (_node) {
-  console.log(_node.data);
-  _(this).extend(_node.data);
-};
