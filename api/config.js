e386f3860ab2bced4d0449d4e2790ee5a77347cb
Cristina <ce@tsl.io>
2016-08-15 13:11:09 -0400

    removed unused variables in api/config.js

--- api/config.js
+++ api/config.js
@@ -1,13 +1,9 @@
 'use strict';
 
 var nconf = require('nconf');
-var auth = '';
-
-// If your database is not using authenticaion, comment the three lines below
 
 var USERNAME = 'neo4j';   // use your username
 var PASSWORD = '123123';    // use your password
-var auth = USERNAME + ':' + PASSWORD + '@'
 
 nconf.env(['PORT', 'NODE_ENV'])
   .argv({
@@ -34,7 +30,6 @@ nconf.env(['PORT', 'NODE_ENV'])
     'USERNAME': USERNAME,
     'PASSWORD' : PASSWORD,
     'neo4j': 'local',
-    // 'neo4j-local': 'http://' + auth + 'localhost:7474',
     'neo4j-local': 'bolt:http://localhost:7687',
     'neo4j-remote': 'bolt:http://162.243.100.222:7687',
     'base_url': 'http://localhost:3000',
