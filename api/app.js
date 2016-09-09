f8b91ef518b66b95fbdde015abd847de3954e235
Janusz Kacalak <jkacalak@gmail.com>
2016-09-09 15:40:00 +0200

    NT-123: Express: Fix "delete rating" API enable CORS on DELETE Fix parsing of movie ID

--- api/app.js
+++ api/app.js
@@ -27,7 +27,7 @@ subpath.use(methodOverride());
 subpath.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
