819dfcdf1c751212ec0dc21e7d2a11fc1494514b
Cristina <ce@tsl.io>
2016-09-12 09:59:39 -0400

    default should be express

--- web/config/settings.example.js
+++ web/config/settings.example.js
@@ -1,5 +1,7 @@
 'use strict';
+// Express: http://localhost:3000/api/v0
+// Flask: http://localhost:5000/api/v0
 
 module.exports = {
-  apiBaseURL: 'http://localhost:5000/api/v0'
+  apiBaseURL: 'http://localhost:3000/api/v0' 
 };
