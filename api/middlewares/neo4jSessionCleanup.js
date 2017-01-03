d98cee0bf2d2bc11888c550637873d3eb3673eae
Lucas Jovencio <lucasjovencio@outlook.com.br>
2017-01-03 20:07:38 -0200

    clear
    Clean for new project

--- api/middlewares/neo4jSessionCleanup.js
+++
@@ -1,9 +0,0 @@
-module.exports = function neo4jSessionCleanup(req, res, next) {
-  res.on('finish', function () {
-    if(req.neo4jSession) {
-      req.neo4jSession.close();
-      delete req.neo4jSession;
-    }
-  });
-  next();
-};
--- api/routes/index.js
+++
@@ -1,6 +0,0 @@
-// convenience wrapper around all other files:
-exports.users = require('./users');
-exports.site = require('./site');
-exports.people = require('./people');
-exports.movies = require('./movies');
-exports.genres = require('./genres');
