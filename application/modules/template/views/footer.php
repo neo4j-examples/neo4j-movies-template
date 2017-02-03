e2cdda4dd13e2b9af0a20d2e4e4a46f768bf9b6d
root <lucasjovencio@outlook.com.br>
2017-02-03 00:40:26 -0200

    load javascript for modules

--- application/modules/template/views/footer.php
+++ application/modules/template/views/footer.php
@@ -3,4 +3,15 @@
     <footer>
 
     </footer>
+<?php 
+	$modulo = $this->uri->segment(1);
+	if($modulo === NULL || $modulo === "home" || $modulo === 'movie' || $modulo === 'person'){
+?>
+	<script src="/assets/node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="/assets/node_modules/lightslider/src/js/lightslider.js"></script>
+	<script src="/assets/js/script_jquery_lightSlider.js"></script>
+	<script src="/assets/js/bootstrap.min.js"></script>
+<?php
+	}
+?>
 </html>
