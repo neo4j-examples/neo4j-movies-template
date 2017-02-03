b3ed7570f1e42d50335b92218edce946ac9ab712
root <lucasjovencio@outlook.com.br>
2017-02-02 23:56:39 -0200

    remove index.php line 38

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
