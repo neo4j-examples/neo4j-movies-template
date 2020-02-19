9e142ba482a6725ecb56086d6f45b7aaed222681
tylerstephens814 <tylerstephens814@gmail.com>
2020-02-19 13:47:23 -0700

    Clean up file

--- web/config/settings.example.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-// Express: http://localhost:3000/api/v0
-// Flask: http://localhost:5000/api/v0
-
-module.exports = {
-  apiBaseURL: 'http://localhost:3000/api/v0' 
-};
--- web/src/utils/ErrorUtils.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-import humps from 'humps';
-
-export default class ErrorUtils {
-  static getApiErrors(apiError) {
-    return humps.camelizeKeys(_.get(apiError, 'data', {})); 
-  }
-}
