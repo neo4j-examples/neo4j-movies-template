7a57bde271eab3069dd8210a90b191ef2f60806d
tylerstephens814 <tylerstephens814@gmail.com>
2020-02-18 21:54:05 -0700

    Commented out routes

--- api/app.js
+++ api/app.js
@@ -63,23 +63,23 @@ api.use(neo4jSessionCleanup);
 
 //api routes
 api.post('/register', routes.users.register);
-api.post('/login', routes.users.login);
 api.get('/users/me', routes.users.me);
-api.get('/movies', routes.movies.list);
-api.get('/movies/recommended', routes.movies.getRecommendedMovies);
-api.get('/movies/rated', routes.movies.findMoviesRatedByMe);
-api.get('/movies/:id',  routes.movies.findById);
-api.get('/movies/genre/:id',  routes.movies.findByGenre);
-api.get('/movies/daterange/:start/:end', routes.movies.findMoviesByDateRange);
-api.get('/movies/directed_by/:id', routes.movies.findMoviesByDirector);
-api.get('/movies/acted_in_by/:id', routes.movies.findMoviesByActor);
-api.get('/movies/written_by/:id', routes.movies.findMoviesByWriter);
-api.post('/movies/:id/rate', routes.movies.rateMovie);
-api.delete('/movies/:id/rate', routes.movies.deleteMovieRating);
-api.get('/people', routes.people.list);
-api.get('/people/:id', routes.people.findById);
-api.get('/people/bacon', routes.people.getBaconPeople);
-api.get('/genres', routes.genres.list);
+// api.post('/login', routes.users.login);
+// api.get('/movies', routes.movies.list);
+// api.get('/movies/recommended', routes.movies.getRecommendedMovies);
+// api.get('/movies/rated', routes.movies.findMoviesRatedByMe);
+// api.get('/movies/:id',  routes.movies.findById);
+// api.get('/movies/genre/:id',  routes.movies.findByGenre);
+// api.get('/movies/daterange/:start/:end', routes.movies.findMoviesByDateRange);
+// api.get('/movies/directed_by/:id', routes.movies.findMoviesByDirector);
+// api.get('/movies/acted_in_by/:id', routes.movies.findMoviesByActor);
+// api.get('/movies/written_by/:id', routes.movies.findMoviesByWriter);
+// api.post('/movies/:id/rate', routes.movies.rateMovie);
+// api.delete('/movies/:id/rate', routes.movies.deleteMovieRating);
+// api.get('/people', routes.people.list);
+// api.get('/people/:id', routes.people.findById);
+// api.get('/people/bacon', routes.people.getBaconPeople);
+// api.get('/genres', routes.genres.list);
 
 //api error handler
 api.use(function(err, req, res, next) {
